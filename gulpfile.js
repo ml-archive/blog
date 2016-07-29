80214725ae33baaa06f60c81e8ece13a555192ae
Dennis Haulund Nielsen <dhni@Denniss-MacBook-Pro.local>
2016-07-29 12:49:52 +0200

    Simplify gulpfile, and move to postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
-var scss = require('gulp-sass');
+var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var autoprefixer = require('gulp-autoprefixer');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 
 var config = {
 	styles: {
@@ -16,30 +17,26 @@ var config = {
 gulp.task('styles', function() {
 	return gulp.src(config.styles.inFiles)
 			.pipe(sourcemaps.init())
-			.pipe(scss({
+			.pipe(sass({
 				includePaths: [
 					'./themes/nodes/node_modules/foundation-sites/scss',
 					'./themes/nodes/node_modules/utility-opentype/css'
 				]
-			}).on('error', scss.logError))
+			}).on('error', sass.logError))
+			.pipe(postcss([
+				autoprefixer()
+			]))
 			.pipe(sourcemaps.write())
-			.pipe(autoprefixer())
 			.pipe(gulp.dest(config.styles.outFiles));
 });
+
 gulp.task('styles:watch', function() {
 	gulp.watch(config.styles.watchPattern, ['styles']);
-});
-
-gulp.task('clean', function() {
-	// DEL
 });
 
 gulp.task('browserSync', function() {
 	browserSync.init({
 		files: ['public/**/*.*'],
-		// server: {
-		// 	baseDir: 'public'
-		// },
 		proxy: 'http://localhost:4000/blog'
 	})
 });
