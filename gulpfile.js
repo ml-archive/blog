25e5937514d43c0db43459d7064ca183ff8c9b12
Dennis Haulund Nielsen <dhni@Denniss-MacBook-Pro.local>
2016-08-29 17:02:03 +0200

    remove unused modules from the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -15,8 +15,6 @@ var autoprefixer = require('autoprefixer');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 
-var shell = require('shelljs');
-
 var config = {
 	styles: {
 		inFiles: './themes/nodes/source/_scss/*.scss',
